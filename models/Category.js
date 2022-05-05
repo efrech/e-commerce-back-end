050c719415f3a79f042f832138ac2ab91615188f
Evelyn Frech <evelynmariafrech@gmail.com>
2022-05-04 21:52:31 -0400

    create: Category, Product, ProductTag, Tag models. Add model associations

--- models/Category.js
+++ models/Category.js
@@ -2,11 +2,22 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
+// Initialize Category model (table) by extending off Sequelize's Model class
 class Category extends Model {}
 
+// set up fields and rules for Category model
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
